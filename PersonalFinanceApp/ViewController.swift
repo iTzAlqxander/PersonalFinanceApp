0f2d096b323e5b706a6b34454fee2e941570a02b
Alexander Masztak <itzalqxander@gmail.com>
2022-06-20 08:48:48 -0500

    Initial Commit

---
+++ PersonalFinanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalFinanceApp
+//
+//  Created by Alexander Masztak on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
