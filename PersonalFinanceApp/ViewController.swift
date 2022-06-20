c0a5a3176960fc4525e4e152b98c749fd0b193bb
Alexander Masztak <itzalqxander@gmail.com>
2022-06-20 11:43:56 -0500

    adding

--- PersonalFinanceApp/ViewController.swift
+++ PersonalFinanceApp/ViewController.swift
@@ -18,21 +18,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var calculateButton: UIButton!
     
-    var userStartingAmount = 0
-    var userYears = 0
-    var userReturnRate = 0
-    var userAdditionalContributions = 0
-    var userStartEnd = 1
-    var userMonthYear = 1
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func whenCalculatePressed(_ sender: UIButton) {
         
+        let userSStartingAmount = startingAmount.text ?? ""
+        let userIOStartingAmount = Int(userSStartingAmount)
+        let userIStartingAmount = Int(userIOStartingAmount ?? 0)
+        //Stores the user's starting amount in the variable userIStartingAmount
+        //print(userIStartingAmount)
         
+        let userSYears = years.text ?? ""
+        let userIOYears = Int(userSYears)
+        let userIYears = Int(userIOYears ?? 0)
+        //Stores the user's # of years to invest in the variable userIYears
+        //print(userIYears)
         
+        let userSReturnRate = returnRate.text ?? ""
+        let userIOReturnRate = Double(userSReturnRate)
+        let userIReturnRate = Double(userIOReturnRate ?? 0.0)
+        //Stores the user's return  rate in the  variable userIReturnRate
+        //print(userIReturnRate)
+        
+        let userSAdditionalContributions = additionalContributions.text ?? ""
+        let userIOAdditionalContributions = Int(userSAdditionalContributions)
+        let userIAdditionalContributions = Int(userIOAdditionalContributions ?? 0)
+        //Stores the user's additional contributions in the variable userIAdditionalContributions
+        //print(userIAdditionalContributions)
+        
+        let inside = 1 + userIReturnRate / 
     }
     
 }
