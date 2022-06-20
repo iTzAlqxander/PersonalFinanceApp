761126a10e193cc1a7942342f5d663e9204108c4
Alexander Masztak <itzalqxander@gmail.com>
2022-06-20 09:17:31 -0500

    main outline finished

--- PersonalFinanceApp/ViewController.swift
+++ PersonalFinanceApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startingAmount: UITextField!
+    @IBOutlet weak var years: UITextField!
+    
+    @IBOutlet weak var returnRate: UITextField!
+    @IBOutlet weak var additionalContributions: UITextField!
+    @IBOutlet weak var startEnd: UISwitch!
+    @IBOutlet weak var monthYear: UISwitch!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    var userStartingAmount = 0
+    var userYears = 0
+    var userReturnRate = 0
+    var userAdditionalContributions = 0
+    var userStartEnd = 1
+    var userMonthYear = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-//test
-
+    
+    @IBAction func whenCalculatePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
 }
 
